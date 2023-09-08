ddedd2b1c43779f03ec321bbf5faeedcf06f82c1
Fernanda Sene <fehsene@gmail.com>
2023-09-08 10:55:03 -0300

    feat: criando api com fastify e prisma

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
+
+const app = fastify()
+app.register(cors, {
+  origin: true, // todas as urls de front-end poderão acassar esse back-end
+})
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
